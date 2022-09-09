2f873a35a74112527802beb2a30f6ec5a057533e
freetimezp <kraftukrnet@ukr.net>
2022-09-09 19:03:20 +0300

    02. js-e-commerce-02. Install Sanity.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,14 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>E-Com 02 App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-
     </div>
-  )
-}
+  );
+};
 
 export default Home
