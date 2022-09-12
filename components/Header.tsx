9453f88d9ddf26c130c4213394ca92bbbc2e206e
freetimezp <kraftukrnet@ukr.net>
2022-09-12 19:56:22 +0300

    08. js-e-commerce-02. Basket Component and display number items in basket.

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import {MagnifyingGlassIcon, ShoppingBagIcon, UserIcon} from "@heroicons/react/24/outline";
+import {selectBasketItems} from "../redux/basketSlice";
+import {useSelector} from "react-redux";
+
 
 function Header() {
     const session = false;
+    const items = useSelector(selectBasketItems);
 
     return (
         <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-[#e7ecee] p-4">
@@ -30,7 +34,9 @@ function Header() {
                 <Link href="/checkout">
                     <div className="relative cursor-pointer">
                         <span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center
-                        rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white">5</span>
+                        rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white">
+                            {items.length}
+                        </span>
                         <ShoppingBagIcon className="headerIcon" />
                     </div>
                 </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import {Tab} from "@headlessui/react";
 import Header from '../components/Header';
 import Landing from '../components/Landing';
 import Product from '../components/Product';
+import Basket from '../components/Basket';
 
 import {fetchCategories} from "../utils/fetchCategories";
 import {fetchProducts} from "../utils/fetchProducts";
@@ -31,6 +32,8 @@ const Home = ({categories, products}: Props) => {
             </Head>
 
             <Header />
+
+            <Basket />
 
             <main className="relative h-[200vh] bg-[#E7ECEE]">
                 <Landing />
