4b09b446d4dc21a8392d912556a746b00cc2cae0
freetimezp <kraftukrnet@ukr.net>
2022-09-14 20:29:30 +0300

    13. js-e-commerce-02. Success page UI - 02. Display products and total summary.

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
-import {CheckIcon} from "@heroicons/react/24/outline";
+import {CheckIcon, ChevronDownIcon, ChevronUpIcon, ShoppingCartIcon} from "@heroicons/react/24/outline";
 import {useRouter} from "next/router";
 
-function Success() {
+import Currency from "react-currency-formatter";
+
+import Button from "../components/Button";
+import {useMediaQuery} from "react-responsive";
+import {GetServerSideProps} from "next";
+import {fetchLineItems} from "../utils/fetchLineItems";
+
+interface Props {
+    products: StripeProduct[]
+}
+
+function Success({products}: Props) {
+    //console.log(products);
     const router = useRouter();
     const {session_id} = router.query;
+    const [mounted, setMounted] = useState(false);
+    const [showOrderSummary, setShowOrderSummary] = useState(false);
+
+    const subtotal = products.reduce((acc, product) => acc + product.price.unit_amount / 100, 0);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    const isTableOrMobile = useMediaQuery({query: "(max-width: 1024px)"});
+    const showOrderSummaryCondition = isTableOrMobile ? showOrderSummary : true;
+
+    const handleShowOrderSummary = () => {
+        !showOrderSummary ? setShowOrderSummary(true) : setShowOrderSummary(false);
+        //setShowOrderSummary(!showOrderSummary);
+    }
 
     return (
         <div>
@@ -25,8 +53,8 @@ function Success() {
                 </Link>
             </header>
 
-            <main>
-                <section className="order-2 mx-auto max-w-xl pb-12 lg:mx-0 lg:max-w-none lg:pr-16 lg:pt-16 xl:pl-16 2xl:pl-44">
+            <main className="grid grid-col-1 lg:grid-cols-9">
+                <section className="order-2 mx-auto max-w-xl pb-12 lg:mx-0 lg:max-w-none lg:col-span-5 lg:pr-16 lg:pt-16 xl:pl-16 2xl:pl-44">
                     <Link href="/">
                         <div className="relative ml-14 hidden h-24 w-12 cursor-pointer transition lg:inline-flex">
                             <Image src="https://rb.gy/vsvv2o" layout="fill" objectFit="contain" />
@@ -70,13 +98,112 @@ function Success() {
                         </p>
                     </div>
 
-                    <div>
+                    <div className="mx-4 flex flex-col items-center justify-between text-sm lg:ml-14 lg:flex-row">
                         <p className="hidden lg:inline">Need help? Contact Us.</p>
+                        {mounted && (
+                            <Button
+                                title="Continue shopping"
+                                onClick={() => router.push("/")}
+                                width={isTableOrMobile ? 'w-full' : ''}
+                                padding="py-4"
+                            />
+                        )}
                     </div>
                 </section>
+
+                {mounted && (
+                    <section className="overflow-y-scroll border-y border-l border-gray-300 bg-[#FAFAFA] lg:order-2 lg:col-span-4
+                        lg:h-screen lg:border-y-0
+                    ">
+                        <div className={`w-full ${showOrderSummaryCondition && "border-b"} border-gray-300 text-sm lg:hidden`}>
+                            <div className="mx-auto flex max-w-xl items-center justify-between px-4 py-6">
+                                <button onClick={handleShowOrderSummary} className="flex items-center space-x-2">
+                                    <ShoppingCartIcon className="w-6 h-6" />
+                                    <p>Show order summary</p>
+                                    {showOrderSummaryCondition ? (
+                                        <ChevronUpIcon className="w-4 h-4" />
+                                    ) : (
+                                        <ChevronDownIcon className="w-4 h-4" />
+                                    )}
+                                </button>
+
+                                <p className="text=xl font-medium text-black">
+                                    {<Currency quantity={subtotal + 20} />}
+                                </p>
+                            </div>
+                        </div>
+
+                        {showOrderSummaryCondition && (
+                            <div className="mx-auto max-w-xl divide-y border-gray-300 px-4 py-4 lg:mx-0 lg:max-w-lg lg:px-10 lg:py-16">
+                                <div className="space-y-4 pb-4">
+                                    {products.map((product) => (
+                                        <div key={product.id} className="flex items-center space-x-4 text-sm font-medium">
+                                            <div className="relative flex h-16 w-16 items-center justify-center rounded-md border
+                                                border-gray-300 bg-[#F1F1F1] text-xs text-white
+                                            ">
+                                                <div className="relative h-7 w-7 animate-bounce rounded-md">
+                                                    <Image src="https://rb.gy/vsvv2o" layout="fill" objectFit="contain" />
+                                                </div>
+                                                <div className="absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center
+                                                    rounded-full bg-[gray] text-xs
+                                                ">
+                                                    {product.quantity}
+                                                </div>
+                                            </div>
+                                            <p className="flex-1">{product.description}</p>
+                                            <p>
+                                                <Currency quantity={product.price.unit_amount / 100} currency={product.currency} />
+                                            </p>
+                                        </div>
+                                    ))}
+                                </div>
+
+                                <div className="space-y-1 py-4">
+                                    <div className="flex justify-between text-sm">
+                                        <p className="text-[gray]">Subtotal</p>
+                                        <p className="font-medium">
+                                            <Currency quantity={subtotal} />
+                                        </p>
+                                    </div>
+                                    <div className="flex justify-between text-sm">
+                                        <p className="text-[gray]">Discount</p>
+                                        <p className="text-[gray]"></p>
+                                    </div>
+                                    <div className="flex justify-between text-sm">
+                                        <p className="text-[gray]">Shipping</p>
+                                        <p className="font-medium">
+                                            <Currency quantity={20} currency="USD" />
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div className="flex justify-between pt-4">
+                                    <p>Total</p>
+                                    <p className="flex items-center gap-x-2 text-xs text-[gray]">
+                                        USD
+                                        <span className="text-xl font-medium text-black">
+                                            <Currency quantity={subtotal + 20} />
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        )}
+                    </section>
+                )}
             </main>
         </div>
     );
 }
 
 export default Success;
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({query}) => {
+    const sessionId = query.session_id as string;
+    const products = await fetchLineItems(sessionId);
+
+    return {
+        props: {
+            products,
+        }
+    }
+};
